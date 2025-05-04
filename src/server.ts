1a80b7e5840cc2a3219d9a01e35ca2abad2442f0
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-04 19:34:13 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
 app.listen({ port: 3333 }).then(() => {
-    console.log('HTTP Server Running')
+  console.log('HTTP Server Running')
 })
