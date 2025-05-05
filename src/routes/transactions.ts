10a1f6f1c7e0ce5e65173f7d717d95f8e106b5c9
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-05 20:45:15 -0300

    feat: create get transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -14,6 +14,20 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   })
 
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
