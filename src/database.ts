073a3dd54980716531969f28d861e03f929b3f69
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-05 14:49:14 -0300

    feat: add .env for configure database

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+const DATABASE_URL = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : './db/app.db'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
