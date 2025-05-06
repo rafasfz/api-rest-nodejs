20f3961055fff4d075d572353e5ac3e849f3f8f5
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-06 15:43:48 -0300

    feat: create tests for list transactions route

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,24 +1,53 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, expect, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-const TRANSACTIONS_ROUTES_PATH = '/transactions/'
+  const TRANSACTIONS_ROUTES_PATH = '/transactions/'
 
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post(TRANSACTIONS_ROUTES_PATH)
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  const transactionBody = {
+    title: 'New transaction',
+    amount: 5000,
+    type: 'credit',
+  }
+
+  it('shouble be able to user can create a new transaction', async () => {
+    await request(app.server)
+      .post(TRANSACTIONS_ROUTES_PATH)
+      .send(transactionBody)
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post(TRANSACTIONS_ROUTES_PATH)
+      .send(transactionBody)
+
+    const cookies = createTransactionResponse.get('Set-Cookie') as string[]
+
+    const listTransactionsResponse = await request(app.server)
+      .get(TRANSACTIONS_ROUTES_PATH)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        id: expect.any(String),
+        title: transactionBody.title,
+        amount:
+          transactionBody.type === 'credit'
+            ? transactionBody.amount
+            : -transactionBody.amount,
+        created_at: expect.any(String),
+      }),
+    ])
+  })
 })
